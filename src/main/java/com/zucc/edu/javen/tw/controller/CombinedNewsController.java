3771ffa7ca3660873a4d235eea316c090fdb69f3
Tender2233 <tengda2233@126.com>
2020-06-05 15:51:01 +0800

    pull 准备

--- src/main/java/com/zucc/edu/javen/tw/controller/CombinedNewsController.java
+++ src/main/java/com/zucc/edu/javen/tw/controller/CombinedNewsController.java
@@ -1,7 +1,5 @@
 package com.zucc.edu.javen.tw.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,12 +7,12 @@ public class CombinedNewsController {
     NewsController dataController = new NewsController();
 
 
-    @PostMapping("/combine/all")
-    public JSONObject getAllNewsList() {
-        JSONObject wangyi = dataController.getWangyixinwenNews("");
-        JSONObject kr36 = dataController.get36keNews("");
-
-        return wangyi;
-    }
+//    @PostMapping("/combine/all")
+//    public JSONObject getAllNewsList() {
+//        JSONObject wangyi = dataController.getWangyixinwenNews("");
+//        JSONObject kr36 = dataController.get36keNews("");
+//
+//        return wangyi;
+//    }
 
 }
